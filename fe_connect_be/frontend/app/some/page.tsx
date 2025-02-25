cd1d0e76a579dfa5b9f0f3be990042b7613ab012
TaiChi <anothai.0978452316@gmail.com>
2025-02-26 00:53:17 +0700

    feat: nothing but I forgot something connect fe with be

---
+++ fe_connect_be/frontend/app/some/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Some() {
+  return (
+    <>
+      <h1>Form Some</h1>
+      <form >
+        <input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
+  )
+}
