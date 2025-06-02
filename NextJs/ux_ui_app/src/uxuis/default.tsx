8010dde7ccf0bf6c771836fcef871ea31a1accf2
TaiChi <anothai.0978452316@gmail.com>
2025-06-02 17:09:39 +0700

    feat: try to toggle switching ux ui product

---
+++ NextJs/ux_ui_app/src/uxuis/default.tsx
@@ -0,0 +1,10 @@
+export default function DefaultUXUI() {
+  return (
+    <nav className="bg-gray-200 text-black p-4">
+      <ul className="flex gap-6 justify-center">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </nav>
+  );
+}
