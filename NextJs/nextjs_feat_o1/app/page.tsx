ee9da21e49aea652719a01f2a446910302121c83
TaiChi <anothai.0978452316@gmail.com>
2025-06-08 18:03:36 +0700

    chore: remove unnecessary newline in page.tsx

--- NextJs/nextjs_feat_o1/app/page.tsx
+++ NextJs/nextjs_feat_o1/app/page.tsx
@@ -1,7 +1,7 @@
 import Nav from "@/app/components/layout/Nav"
 import Section from "@/app/components/layout/Section"
 import Footer from "@/app/components/layout/Footer"
-
+ 
 export default function Home() {
   return (
     <>
