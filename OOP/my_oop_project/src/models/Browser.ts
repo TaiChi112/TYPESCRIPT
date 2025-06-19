094cc06e7cb0b11bd25ba1ab0d00c75c2ea9219a
TaiChi <anothai.0978452316@gmail.com>
2025-06-20 04:13:25 +0700

    feat: add User and Browser models with Account class extending User

---
+++ OOP/my_oop_project/src/models/Browser.ts
@@ -0,0 +1,31 @@
+export class Browser {
+    private browser_name: string;
+    private version: string;
+    private url: string;
+
+    constructor(browser_name: string, version: string, url: string) {
+        this.browser_name = browser_name;
+        this.version = version;
+        this.url = url;
+    }
+
+    getUrl(): string {
+        return this.url;
+    }
+
+    getBrowserName(): string {
+        return this.browser_name;
+    }
+
+    getVersion(): string {
+        return this.version;
+    }
+
+    setUrl(newUrl: string): void {
+        this.url = newUrl;
+    }
+
+    showInfo(): string {
+        return `Browser Name: ${this.browser_name}, Version: ${this.version}, Current URL: ${this.url}`;
+    }
+}
